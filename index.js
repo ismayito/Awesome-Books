afe171a0c308f45b885ba469f37953d608a0098c
Afimia Fidelis <38649892+Izebiafe@users.noreply.github.com>
2023-08-09 08:38:22 +0000

    fixed Eslint errors

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 const booksArrays = [];
 let booksObject = {};
+let booksFromStorage = JSON.parse(localStorage.getItem('bookStore')) || [];
 
-//function to add book
-function addBook(e) {
-  e.preventDefault();
-  const titleInput = document.getElementById("title").value;
-  const authorInput = document.getElementById("author").value;
-  const id = Math.floor(Math.random() * 1000);
-  booksObject = { author: authorInput, title: titleInput, id: id };
-  booksArrays.push(booksObject);
-  displayBook(booksObject);
-  localStorage.setItem("bookStore", JSON.stringify(booksArrays));
+//  function to delete book
+
+function deleteBook(button) {
+  const btnId = Number(button.getAttribute('data-id'));
+  const newBooks = booksFromStorage.filter((book) => book.id === btnId);
+  localStorage.setItem('bookStore', JSON.stringify(newBooks));
+  booksFromStorage = newBooks;
 }
 
-const container = document.getElementById("books");
+const container = document.getElementById('books');
 
-//function to display book
+// function to display book
+
 function displayBook(booksObject) {
-  const bookAuthor = document.createElement("p");
-  const removeButton = document.createElement("button");
-  const horizontalLine = document.createElement("hr");
+  const bookAuthor = document.createElement('p');
+  const removeButton = document.createElement('button');
+  const horizontalLine = document.createElement('hr');
   bookAuthor.innerText = `${booksObject.title} by ${booksObject.author}`;
-  removeButton.innerText = "Remove";
-  removeButton.className = "button";
-  removeButton.setAttribute("data-id", booksObject.id);
-  removeButton.addEventListener("click", function () {
+  removeButton.innerText = 'Remove';
+  removeButton.className = 'button';
+  removeButton.setAttribute('data-id', booksObject.id);
+  removeButton.addEventListener('click', () => {
     deleteBook(removeButton);
     container.removeChild(removeButton);
     container.removeChild(bookAuthor);
@@ -35,19 +34,20 @@ function displayBook(booksObject) {
   container.appendChild(horizontalLine);
 }
 
-let booksFromStorage = JSON.parse(localStorage.getItem("bookStore")) || [];
+// function to add book //
 
-//function to delete book
-function deleteBook(button) {
-  const btnId = Number(button.getAttribute("data-id"));
-  const newBooks = booksFromStorage.filter((book) => book.id === btnId);
-  localStorage.setItem("bookStore", JSON.stringify(newBooks));
-  booksFromStorage = newBooks;
+// eslint-disable-next-line no-unused-vars
+function addBook(e) {
+  e.preventDefault();
+  const titleInput = document.getElementById('title').value;
+  const authorInput = document.getElementById('author').value;
+  const id = Math.floor(Math.random() * 1000);
+  booksObject = { author: authorInput, title: titleInput, id };
+  booksArrays.push(booksObject);
+  displayBook(booksObject);
+  localStorage.setItem('bookStore', JSON.stringify(booksArrays));
 }
 
-//display books;
-function loopBooks() {
-  booksFromStorage.map((book) => {
-    displayBook(book);
-  });
-}
+// Render Books
+
+booksFromStorage.map((book) => displayBook(book));
